531949e9e85ab43e46ec7c72f7cd5ccf9e285f82
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2025-09-30 14:55:05 -0400

    blahhh organization

---
+++ Assets/Code/player scripts/mouseInputs.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class mouseInputs : MonoBehaviour, IOnMouseDown
+{
+
+    public int counter = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnMouseDown() { //uses an Interface, has to be detailed by manager?
+        if (counter == 0)
+        {
+
+            counter = 1;
+        }
+        else
+        {
+
+            counter = 0;
+        }
+    }
+    
+
+}
