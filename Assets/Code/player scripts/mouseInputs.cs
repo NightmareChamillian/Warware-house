009a7f4641eb265b03c15fbb56e3ec65ac36f13e
abaldome <abaldome@terpmail.umd.edu>
2025-10-02 22:02:35 -0400

    Created variables to track mouse delta, which print to unity console. I included both legacy and current input system depending on what we decide to go with.

--- Assets/Code/player scripts/mouseInputs.cs
+++ Assets/Code/player scripts/mouseInputs.cs
@@ -1,19 +1,34 @@
 using UnityEngine;
+using System;
+using UnityEngine.InputSystem;
 
 public class MouseInputs : MonoBehaviour, IOnMouseDown
 {
-
+    public float mouseX;
+    public float mouseY;
+    public Vector2 mouse;
     public int counter = 0;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Legacy Input System
+        // mouseX = Input.GetAxis("Mouse X");
+        // mouseY = Input.GetAxis("Mouse Y");
 
+
+        // New Input System
+        mouse = Mouse.current.delta.ReadValue();
+        mouseX = mouse.x;
+        mouseY = mouse.y;
+
+        Debug.Log("mouse X: " + mouseX);
+        Debug.Log("mouse Y: " + mouseY);
     }
 
     public void OnMouseDown() { //uses an Interface, has to be detailed by manager?
