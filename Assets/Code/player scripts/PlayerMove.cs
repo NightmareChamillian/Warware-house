a81aee701402ffe55c1c0da522804eb4f838f09b
Millian F <96703882+NightmareChamillian@users.noreply.github.com>
2025-10-05 15:25:18 -0400

    Players have a rudimentary kinematic controller
    it feels a little slippery but thats mainly a tuning issue

--- Assets/Code/player scripts/PlayerMove.cs
+++ Assets/Code/player scripts/PlayerMove.cs
@@ -1,70 +1,142 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
+using System;
 public class PlayerMove : MonoBehaviour
 {
 
+    //added to forBack/sideSide each time. in the future will be subject to change with state.
+    private float sideSpeed = 0.1f; 
+    private float forSpeed = 0.1f; 
+    public float maxWalkSpeed = 26; //what's the MOST speed we can get up to?
+    
 
 
-    private int speed = 13;
-
-
-    //realizing we don't need these variables if we can just shove them in start()
-    public Key forwardMapping = Key.UpArrow;
+    //realizing we don't need these variables since we can just shove them in start()
+    //public Key forwardMapping = Key.UpArrow;
     public KeyControl forwardKey;
-
-    //public Key backMove = ;
     public KeyControl backKey;
     public KeyControl leftKey;
     public KeyControl rightKey;
+
+    private bool[] nowPressed = {false,false,false,false}; //same as below but for the now
+    private bool[] wasPressed = {false,false,false,false}; //W,A,S,D array we use to keep track of keys having JUST been pressed, used for stopping
+
+
+    //these two values are our speed each frame.
+    private float forBack = 0;
+    private float sideSide = 0;
 
     // setup function
     void Start()
     {
         //assign all our keycontrols
-      forwardKey = Keyboard.current[forwardMapping];
-      backKey = Keyboard.current[Key.DownArrow];
+      forwardKey = Keyboard.current[Key.W];
+      backKey = Keyboard.current[Key.S];
 
-      leftKey = Keyboard.current[Key.LeftArrow];
-      rightKey = Keyboard.current[Key.RightArrow];
+      leftKey = Keyboard.current[Key.A];
+      rightKey = Keyboard.current[Key.D];
     }
 
     // Update is called once per frame
     void Update()
     {
-        //check all the keycontrol objects
+        //check all the keycontrol objects, update our info arrays
 
         if(forwardKey.isPressed){
-           forBack += speed; } 
+            forBack += forSpeed;
+           nowPressed[0] = true; }
+        else{
+            nowPressed[0] = false;
+        }
+
         if(backKey.isPressed){
-            forBack -= speed; }
+            forBack -= forSpeed;
+            nowPressed[2] = true;}
+        else{
+            nowPressed[2] = false;
+        }
 
         if(leftKey.isPressed){
-           sideSide -= speed; } 
+            sideSide -= sideSpeed;
+           nowPressed[1] = true;} 
+        else{
+            nowPressed[1] = false;
+        }
 
         if(rightKey.isPressed){
-           sideSide += speed; } 
+            sideSide += sideSpeed;
+           nowPressed[3] = true;} 
+        else{
+            nowPressed[3] = false;
+        }
+
+
+        //give a boost once we've passed the threshhold. this emulates momentum, but in a simpler way.
+        if(forBack > 6){
+            forSpeed = 0.3f; 
+        }
+        if(sideSide > 6){
+            sideSpeed = 0.3f; 
+        }
+        //blehhhh do it for negatives as well
+        if(forBack < 0-6){
+            forSpeed = 0.3f; 
+        }
+        if(sideSide < 0-6){
+            sideSpeed = 0.3f; 
+        }
+
+
+
+        //check to see if we WERE pressing a movement key and no longer are. This will "break" any speed we've acculmulated.
+        if(wasPressed[0] || wasPressed[2]){ //forwards back
+            if(!(nowPressed[0] || nowPressed[2])){ //gotta love demorgansing boolean statements
+                forBack = 0;
+                forSpeed = 0.1f; 
+            }
+        }
+
+        if(wasPressed[1] || wasPressed[3]){ //and again for side-side
+            if(!(nowPressed[1] || nowPressed[3])){ 
+                sideSide = 0;
+                sideSpeed = 0.1f; 
+            }
+        }
+
+
+        //update our wasPressed array for next frame
+        wasPressed[0] = nowPressed[0];
+        wasPressed[1] = nowPressed[1];
+        wasPressed[2] = nowPressed[2];
+        wasPressed[3] = nowPressed[3];
+
+
+        //finally, clamp our movement. this does little right now but will be very important later when we have things like sprinting/aiming/crouching movespeed modifiers
+        forBack = Mathf.Clamp(forBack, 0-maxWalkSpeed, maxWalkSpeed);
+        sideSide = Mathf.Clamp(sideSide, 0-maxWalkSpeed, maxWalkSpeed);
 
         executeMotion();
     }
 
-    private int forBack = 0;
-    private int sideSide = 0;
+
     //private Vector3 velocity;
 
     private void executeMotion(){ // Check our pressed keys, move, and refresh
 
 
         transform.Translate(sideSide * Time.deltaTime, 0, forBack * Time.deltaTime); //stays local to the object
-        refreshKeys();
+        //refreshKeys();
 
     }
 
-    private void refreshKeys(){ //refresh our keypresses for the next frame
+    //depreciated since I added the waspressed and nowpressed arrays which will refresh situationally
 
-        forBack = 0;
-        sideSide = 0;
-    }
+    // private void refreshKeys(){ //refresh our keypresses for the next frame
+
+    //     forBack = 0;
+    //     sideSide = 0;
+    // }
 
     //destroy(ENTITY); //queue for deletion
 }
--- Assets/Code/player scripts/mouseInputs.cs
+++ Assets/Code/player scripts/mouseInputs.cs
@@ -4,17 +4,24 @@ using UnityEngine.InputSystem;
 
 public class MouseInputs : MonoBehaviour, IOnMouseDown
 {
-    public float mouseX;
-    public float mouseY;
-    public Vector2 mouse;
-    public int counter = 0;
+    public float mouseX; //up/down movement
+    public float mouseY; //left/right movement
+    public float mouseSensitivity = 1; //multiplier applied to mouse movements, test before doing anything with this
+    public Vector2 mouse; //vector2 used to grab the x and y components from inputsystem call
+
+    float xRotation = 0;
+    float yRotation = 0;
+
+    public int mouseCounter = 0; //rudimentary counter that changes when clicks happen. Does nothing(?)
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
-        
+    {   
+
+        //lock the cursor so we don't move out of window (but let's not for the time being)
+        //Cursor.lockState = CursorLockMode.Locked; 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         // Legacy Input System
@@ -24,23 +31,38 @@ public class MouseInputs : MonoBehaviour, IOnMouseDown
 
         // New Input System
         mouse = Mouse.current.delta.ReadValue();
-        mouseX = mouse.x;
-        mouseY = mouse.y;
+        mouseX = mouse.x * mouseSensitivity;
+        mouseY = mouse.y * mouseSensitivity;
 
-        Debug.Log("mouse X: " + mouseX);
-        Debug.Log("mouse Y: " + mouseY);
+        //Debug.Log("mouse X: " + mouseX);
+        //Debug.Log("mouse Y: " + mouseY);
+
+        //up down rotation is disabled (for now) because we want it parented to the CAMERA. not the player. this will create a sense of a virtual body when the
+        //player looks down as well as stop them from turning into a hard to hit hotdog when they look straight up. generally good behavior.
+        //why not have the horizontal movement also parented to the camera? partially, to stop them from looking at the back of their playerbody, but also
+        //so we can get the player's angle and direction without having to do too much weird conversion stuff.
+        //I'm not married to this setup btw but IMO it's the best way to do things. - Rowan
+
+        //xRotation -= mouseY;
+        //xRotation = Mathf.clamp(mouseY, 90f, 90f); //clamp the up/down movement, keeps players from breaking their virtual "necks".
+
+        yRotation += mouseX;
+
+
+
+
     }
 
     public void OnMouseDown() { //uses an Interface, has to be detailed by manager?
-        if (counter == 0)
+        if (mouseCounter == 0)
         {
 
-            counter = 1;
+            mouseCounter = 1;
         }
         else
         {
 
-            counter = 0;
+            mouseCounter = 0;
         }
     }
     
