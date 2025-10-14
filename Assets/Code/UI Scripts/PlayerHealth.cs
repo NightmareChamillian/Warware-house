830e848d0012fc3a5d1768b567e0236affc9d925
Arlan Baldomero <abaldome@terpmail.umd.edu>
2025-10-14 14:13:59 -0400

    Added gradient effect to HP
    color of HP bar slowly changes from green to red as health decreases

---
+++ Assets/Code/UI Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Image filledBar;
+    public Gradient healthColorGradient;
+
+    public double minHP, maxHP;
+    public double currentHP;
+
+    void Start()
+    {
+        minHP = 0;
+        maxHP = 100;
+        currentHP = 100;
+    }
+
+
+    void Update()
+    {
+        currentHP -= .01;
+        filledBar.fillAmount = (float)(currentHP / maxHP);
+        filledBar.color = healthColorGradient.Evaluate(filledBar.fillAmount);
+    }
+}
